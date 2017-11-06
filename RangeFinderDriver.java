4aa9a5bc0128a8ee1be359af4fb2263c6644ed3b
CHRIS_GOEBEL <cmgoebel@iupui.edu>
2017-11-05 22:06:42 -0500

    add try catch statement in driver around getDistance() call

--- RangeFinderDriver.java
+++ RangeFinderDriver.java
@@ -4,8 +4,13 @@ class RangeFinderDriver {
         HC_SR04 rangeFinder = new HC_SR04();
 
         while(true) {
-            System.out.println("Distance : " + rangeFinder.getDistance());
-            Thread.sleep(1000);
+            try{
+                System.out.println("Distance : " + rangeFinder.getDistance());
+                Thread.sleep(1000);                
+            } catch (Exception ex) { 
+                ex.printStackTrace();
+            }
+
         }
     }
 }
