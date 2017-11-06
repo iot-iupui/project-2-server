2de3356d45df678506e84f73e652c65c84256513
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-11-05 21:27:54 -0500

    Created new java project

---
+++ project-2-java/src/main/java/org/iupui/iot/project2/server/GetGreeting.java
@@ -0,0 +1,18 @@
+package org.iupui.iot.project2.server;
+
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+
+public class GetGreeting extends CoapResource {
+    public GetGreeting() {
+        super("hello");
+
+        // set display name
+        getAttributes().setTitle("hello");
+    }
+
+    @Override
+    public void handleGET(CoapExchange exchange) {
+        exchange.respond(new HelloPojo("Hello Chris").toString());
+    }
+}
